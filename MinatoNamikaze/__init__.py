23eb338c262aec6d1e9978414ba243fd591742ed
DarkGod14 <63758195+DarkGod14@users.noreply.github.com>
2021-06-26 16:22:45 -0500

    Update __init__.py

--- MinatoNamikaze/__init__.py
+++ MinatoNamikaze/__init__.py
@@ -59,7 +59,7 @@ if ENV:
         raise Exception(
             "Your whitelisted users list does not contain valid integers.")
 
-    INFOPIC = bool(os.environ.get('INFOPIC', False))
+    INFOPIC = bool(os.environ.get('INFOPIC', True))
     EVENT_LOGS = os.environ.get('EVENT_LOGS', None)
     WEBHOOK = bool(os.environ.get('WEBHOOK', False))
     URL = os.environ.get('URL', "")  # Does not contain token
