70ce8735b68389b1f90a3759463cd07c51faa399
ripanrahayu <77609736+ripanrahayu@users.noreply.github.com>
2021-06-28 04:31:50 +0700

    Update sample_config.py

--- MinatoNamikaze/sample_config.py
+++ MinatoNamikaze/sample_config.py
@@ -19,8 +19,8 @@ class Config(object):
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 1657187491  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "DarkGod14"
-    SUPPORT_CHAT = 'MinatoNamikazeSupport'  #Your own group for support, do not add the @
+    OWNER_USERNAME = "Ripan Rhy"
+    SUPPORT_CHAT = 'gemoySupport'  #Your own group for support, do not add the @
     JOIN_LOGGER = -1001253661229  #Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = -1001190806654  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
